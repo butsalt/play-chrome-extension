1b52532977cf3b0001a640185b9523f2c4ad4516
YuHeng Sun <butsalt@163.com>
2017-11-24 11:27:16 +0800

    improve comment on inject

--- inject/background/background.js
+++ inject/background/background.js
@@ -7,7 +7,8 @@
   //  path不用配，即使写了也会被忽略
 
   // cross-origin permission作用
-  //  1. 允许扩展在发起跨域xhr请求时，即使响应不带Access-Control-Allow-Origin也不会被block
+  //  1. 允许扩展在发起跨域xhr时，即使响应不带Access-Control-Allow-Origin也不会被block
+  //     也就是说，如果跨域xhr的响应中带有Access-Control-Allow-Origin，那么即使不使用cross-origin permission，也可以正常获取结果
   //  2. 允许扩展可以在对应站点插入js，css
   chrome.browserAction.onClicked.addListener(function (tab) {
     // 如果对应站点的权限没有获取到，则无法获得地址
